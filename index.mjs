f99ac46665539f0be2597736d2128c80cff20074
Prantik <66416792+prantiknoor@users.noreply.github.com>
2025-10-02 09:20:29 +0600

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,9 +1,24 @@
 import http from 'http';
+import os from 'os';
 import bot from "./bot.mjs";
 
 // This server setup is intended for deployment on render.com, it has no other meaning.
 const PORT = 3000;
 const REDIRECT_URL = 'https://github.com/prantiknoor/p2p-alert-bot';
+
+// Utility function to get server IP
+function getServerIP() {
+  const nets = os.networkInterfaces();
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name]) {
+      // Skip over non-IPv4 and internal (i.e. 127.0.0.1)
+      if (net.family === 'IPv4' && !net.internal) {
+        return net.address;
+      }
+    }
+  }
+  return 'Unknown';
+}
 
 const server = http.createServer((req, res) => {
   if (req.url !== '/health') {
@@ -16,6 +31,10 @@ const server = http.createServer((req, res) => {
   } else if (req.url === '/ping') {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('OK');
+  } else if (req.url === '/ip') {
+    const ip = getServerIP();
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.write(JSON.stringify({ ip }));
   } else {
     res.writeHead(302, { Location: REDIRECT_URL });
   }
